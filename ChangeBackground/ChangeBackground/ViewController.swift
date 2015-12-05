83d975d90a5e763c0c6282c55e2c88aa4c028e71
Venj <ersaclarke@gmail.com>
2015-12-05 20:11:29 +0800

    Small change. Cheat.

--- ChangeBackground/ChangeBackground/ViewController.swift
+++ ChangeBackground/ChangeBackground/ViewController.swift
@@ -13,13 +13,20 @@ import RxCocoa
 class ViewController: UIViewController {
 
     @IBOutlet weak var button: UIButton!
+    var disposeBag: DisposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        _ = button.rx_tap.subscribeNext { [weak self] in
+        /*button.rx_tap.subscribeNext { [weak self] in
             self?.view.backgroundColor = UIColor.blackColor()
-        }
+        }.addDisposableTo(disposeBag)*/
+
+        button.rx_tap.map {
+            return UIColor.greenColor()
+        }.subscribeNext { [weak self] color in
+            self?.view.backgroundColor = color
+        }.addDisposableTo(disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
